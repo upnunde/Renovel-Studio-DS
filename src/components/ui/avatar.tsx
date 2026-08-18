8b1dccb6bc7d45e1a5d25a8249112b7d6a1da9e4
upnunde <upnunde@gmail.com>
2026-08-18 16:41:37 +0900

    feat(ds): design-system v0.1.35

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -126,7 +126,7 @@ function AvatarImage({ className, ...props }: AvatarPrimitive.Image.Props) {
     <AvatarPrimitive.Image
       data-slot="avatar-image"
       className={cn(
-        "aspect-square size-full rounded-full object-cover",
+        "aspect-square size-full rounded-full object-cover shadow-[inset_0_0_0_9999px_var(--black-opacity-10)]",
         className
       )}
       {...props}
